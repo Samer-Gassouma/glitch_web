d6499a1e1a5f9712ac0469243eb1c97158281bcc
Samer Gassouma <samer.samm12@gmail.com>
2024-01-29 14:41:27 +0100

    fixing 15

---
+++ app/all_The_User777/page.tsx
@@ -0,0 +1,35 @@
+"use client"
+import { useState, useEffect } from 'react';
+import { supabase } from '@/utils/supabase/client';
+
+export default function UsersPage() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  async function fetchUsers() {
+    const { data, error } = await supabase
+      .from('users')
+      .select('*');
+
+    if (error) {
+      console.error('Error fetching users:', error);
+      return;
+    }
+
+    setUsers(data);
+  }
+
+  return (
+    <div className="p-4 justify-center items-center flex flex-col mt-10">
+      <h1 className="text-2xl mb-4 ">Users ({users.length})</h1>
+      <ul className="list-disc list-inside overflow-auto h-180">
+        {users.map((user, index) => (
+          <li key={index} className="mb-2">{user.email}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
