5f4f0f0d74722caadef18a128b254fae7623212f
Samer Gassouma <samer.samm12@gmail.com>
2024-01-31 13:20:09 +0100

    log 20

--- app/page.tsx
+++ app/page.tsx
@@ -28,32 +28,37 @@ export default async function Index() {
  
   return (
     <div className="flex-1 w-full flex flex-col items-center">
-      <div className="overflow-y-hidden h-screen flex-1">
+      <div className="overflow-y-hidden h-screen flex-1 bg-black">
      {!user ? 
-         <div className="flex items-center justify-center h-screen">
-          <div className="flex flex-col items-center">
-            <h1 className="text-6xl font-bold mb-4">Welcome to Glitch</h1>
-            <p className="text-xl text-center">
-              A place where you can learn and share your knowledge with others.
+        <div className="flex items-center justify-center h-screen">
+        <div className="flex flex-col items-center">
+          <h1 className="text-6xl font-bold mb-4">Welcome to Glitch</h1>
+          <p className="text-xl text-center">
+            A place where you can learn and share your knowledge with others.
+          </p>
+      
+          <div className="mt-4 p-4 border border-red-500 bg-red-100 text-red-700">
+            <p className="font-bold">Important Notice:</p>
+            <p>All accounts have been deleted. Please create a new account to continue.
+              Don't worry, this won't happen again.
+              <br></br>
+              (Don't forget to check your spam folder for the verification email)
             </p>
-            
-           
-            <span 
-            className="text-xl text-center mt-4"
-            >But you Need to{" "}
-            <a
-            href="/login"
-            className="text-blue-500 hover:underline cursor-pointer font-bold"
-            >Login</a> First </span>
-           
-              <p className="text-xl text-center mt-4 font-bold">
-                  WE ARE STILL IN BETA SO PLEASE REPORT ANY BUGS YOU FIND
-              </p>
-              
-
           </div>
-            
+      
+          <span 
+          className="text-xl text-center mt-4"
+          >But you Need to{" "}
+          <a
+          href="/login"
+          className="text-blue-500 hover:underline cursor-pointer font-bold"
+          >Login</a> First </span>
+      
+          <p className="text-xl text-center mt-4 font-bold">
+            WE ARE STILL IN BETA SO PLEASE REPORT ANY BUGS YOU FIND
+          </p>
         </div>
+      </div>
         :
         <LatestCourses />
       }
@@ -64,7 +69,7 @@ export default async function Index() {
           <a
             href="https://twitter.com/SamerGassouma"
             target="_blank"
-            className="font-bold hover:underline"
+            className="font-bold hover:underline text-white"
             rel="noreferrer"
           >
             ivan
